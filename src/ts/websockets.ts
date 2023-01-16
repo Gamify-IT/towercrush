2606a9e0b760def5b39ef2387b81c6bb6734fb3e
Levi <otterbach.levi@gmail.com>
2023-01-16 16:38:16 +0100

    fix reset bug

--- src/ts/websockets.ts
+++ src/ts/websockets.ts
@@ -24,7 +24,7 @@ export function connect(lobby: string, player: string) {
     stompClientGame.value = Stomp.over(
       new SockJS("/minigames/towercrush/api/v1/connect")
     );
-    playerUUID.value = generateUUID();
+    playerUUID.value = getPlayerUUID();
     console.log("players uuid was: ", playerUUID.value);
     stompClientGame.value.connect(
       { player: player, lobby: lobby, userUUID: playerUUID.value },
@@ -143,23 +143,7 @@ export function clearDeveloperLobby() {
   }
 }
 
-//helpmethode remove it when implement uuid from cookie
-function generateUUID() {
-  let d = new Date().getTime();
-  let d2 =
-    (typeof performance !== "undefined" &&
-      performance.now &&
-      performance.now() * 1000) ||
-    0;
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    let r = Math.random() * 16;
-    if (d > 0) {
-      r = (d + r) % 16 | 0;
-      d = Math.floor(d / 16);
-    } else {
-      r = (d2 + r) % 16 | 0;
-      d2 = Math.floor(d2 / 16);
-    }
-    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-  });
+function getPlayerUUID(): string {
+  const uuid = localStorage.getItem("userId")!;
+  return uuid;
 }
